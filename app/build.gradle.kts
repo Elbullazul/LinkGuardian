ee0450333bf3a3e619478f9ec7ffdd4d5ea26f50
Christian Medel <dev@elbullazul.net>
2024-10-09 15:24:01 -0400

    Remove useless line

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-//    kotlin("jvm") version "2.0.10"
     kotlin("plugin.serialization") version "2.0.10"
 }
 
