e2c492610e3b1e5bbf7e40b997a0f30611dcabe6
Christian Medel <dev@elbullazul.net>
2025-04-08 15:29:30 -0400

    Remove unnecessary objects

--- app/src/main/java/dev/elbullazul/linkguardian/backends/linkwarden/LinkwardenParent.kt
+++
@@ -1,9 +0,0 @@
-package dev.elbullazul.linkguardian.backends.linkwarden
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class LinkwardenParent(
-    val id: Int,
-    val name: String
-)
