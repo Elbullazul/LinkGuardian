1f2d98b1d8b5bdacba00f44d5e1e521e470817ed
Christian Medel <dev@elbullazul.net>
2025-04-18 14:58:56 -0400

    Update collection fragment

--- app/src/main/java/dev/elbullazul/linkguardian/ui/fragments/CollectionFragment.kt
+++ app/src/main/java/dev/elbullazul/linkguardian/ui/fragments/CollectionFragment.kt
@@ -10,11 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import dev.elbullazul.linkguardian.R
+import dev.elbullazul.linkguardian.data.extensions.Describable
 import dev.elbullazul.linkguardian.data.extensions.ParentOfMany
 import dev.elbullazul.linkguardian.data.generic.Collection
 import dev.elbullazul.linkguardian.data.linkwarden.LinkwardenCollection
@@ -22,27 +21,29 @@ import dev.elbullazul.linkguardian.ui.theme.LinkGuardianTheme
 
 @Composable
 fun CollectionFragment(collection: Collection, onClick: (String) -> Unit) {
-    Row {
-        Column {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+    Column(modifier = Modifier.padding(horizontal = 10.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .clickable { onClick(collection.getId()) }
+        ) {
+            Column(modifier = Modifier.weight(1.0f)) {
                 Text(
                     text = collection.name,
-                    style = TextStyle(fontSize = 20.sp),
-                    modifier = Modifier
-                        .padding(vertical = 6.dp)
-                        .weight(1.0f)
-                        .clickable { onClick(collection.getId()) }
+                    color = MaterialTheme.colorScheme.primary,
                 )
-                if (collection is ParentOfMany) {
-                    Text(
-                        color = MaterialTheme.colorScheme.secondary,
-                        text = "${collection.getMemberCount()} ${stringResource(R.string.links_or_link)}"
-                    )
+                if (collection is Describable) {
+                    Text(text = collection.truncatedDescription())
                 }
             }
-//            if (collection is Describable) {
-//                Text(text = collection.truncatedDescription())
-//            }
+
+            if (collection is ParentOfMany) {
+                Text(
+                    color = MaterialTheme.colorScheme.secondary,
+                    text = "${collection.getMemberCount()} ${stringResource(R.string.links_or_link)}"
+                )
+            }
         }
     }
 }
